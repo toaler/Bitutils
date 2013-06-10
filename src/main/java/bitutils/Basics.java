33f751f3b6de090b8e74835bcccb273fb7543e48
Brian Toal <brian.toal@gmail.com>
2013-06-10 15:08:53 -0700

    Added enable right most 0 bit.

--- src/main/java/bitutils/Basics.java
+++ src/main/java/bitutils/Basics.java
@@ -4,4 +4,8 @@ public class Basics {
 	public static int turnOffRightMost1Bit(final int input) {
 		return input & (input - 1);
 	}
+
+	public static int turnOnRightMost1Bit(int input) {
+		return input | (input + 1);
+	}
 }
--- src/test/java/bitutils/BasicsTest.java
+++ src/test/java/bitutils/BasicsTest.java
@@ -17,5 +17,22 @@ public class BasicsTest {
 		
 		input = Basics.turnOffRightMost1Bit(input);
 		assertEquals(0b00000000, input);
+		
+		input = Basics.turnOffRightMost1Bit(input);
+		assertEquals(0b00000000, input);
+	}
+	
+	@Test
+	public void turnOnRightMost1Bit() {
+		int input  = 0b10100111;
+
+		input = Basics.turnOnRightMost1Bit(input);
+		assertEquals(0b10101111, input);
+		
+		input = Basics.turnOnRightMost1Bit(input);
+		assertEquals(0b10111111, input);
+		
+		input = Basics.turnOnRightMost1Bit(input);
+		assertEquals(0b11111111, input);
 	}
 }
