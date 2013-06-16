544ec5c994f9824bda4af21a32567f520a011b5c
Brian Toal <brian.toal@gmail.com>
2013-06-16 00:06:14 -0700

    Added various features, see diff for details.

--- src/main/java/bitutils/Basics.java
+++ src/main/java/bitutils/Basics.java
@@ -1,11 +1,11 @@
 package bitutils;
 
 public class Basics {
-	public static int turnOffRightMost1Bit(final int input) {
+	public static int turnOffRightmost1(final int input) {
 		return input & (input - 1);
 	}
 
-	public static int turnOnRightMost0Bit(final int input) {
+	public static int turnOnRightmost0(final int input) {
 		return input | (input + 1);
 	}
 
@@ -16,4 +16,110 @@ public class Basics {
 	public static int turnOnTrailing0s(final int input) {
 		return input | (input - 1);
 	}
-}
+
+	public static int markPositionOfRightmost0With1(final int input) {
+		return ~input & (input + 1);
+	}
+
+	public static int markPositionOfRightmost1With0(final int input) {
+		return ~input | (input - 1);
+	}
+
+	public static int createWordWith1sAtPositionsOfTrailing0s(final int input) {
+		return ~input & (input - 1);
+	}
+
+	public static int createWordWith0sAtPositionsOfTrailing1s(final int input) {
+		return ~input | (input + 1);
+	}
+
+	public static int markPositionOfRightmost1BitWith1(final int input) {
+		return input & (input * -1);
+	}
+
+	public static int markRightmost1WithContiguous1s(final int input) {
+		return input ^ (input - 1);
+	}
+
+	public static int markRightmost0WithContiguous1s(final int input) {
+		return input ^ (input + 1);
+	}
+
+	public static int clearRightmostContiguousStringOf1s(final int input) {
+		return (markPositionOfRightmost1BitWith1(input) + input) & input;
+	}
+
+	public static boolean isBaseTwoAMultipleOfBaseTwo(final int baseTwoB, final int baseTwoA) {
+		if (baseTwoB < baseTwoA)
+			throw new IllegalArgumentException();
+		
+		if (baseTwoA < 0)
+			throw new IllegalArgumentException();
+		
+		if ((clearRightmostContiguousStringOf1s(baseTwoB) | clearRightmostContiguousStringOf1s(baseTwoA)) == 0){
+			return true;
+		}
+		
+		return false;
+	}
+
+	public static int abs(final int input) {
+		int y = input >> 31;
+		return (input ^ y) - y;
+	}
+
+	public static int nabs(final int input) {
+		int y = input >> 31;
+		return y - (input ^ y);
+	}
+
+	
+	/**
+	 * Computes rounded down average of two unsigned integers without overflow.
+	 */
+	public static int floorAvg(final int x, final int y) {
+		return ((x & y) + ((x ^ y) >>> 1));
+	}
+
+	/**
+	 * Computes rounded up average of two unsigned integers without overflow.
+	 */
+	public static int ceilingAvg(final int x, final int y) {
+		return ((x | y) - ((x ^ y) >>> 1));
+	}
+
+	public static int threeValueCompare(final int x, final int y) {
+		return (lt(y, x) - lt(x, y));
+	}
+
+	public static int eq(final int x, final int y) {
+		return (abs(x - y) - 1) >>> 31;
+	}
+
+	public static int ne(final int x, final int y) {
+		return nabs(x - y) >>> 31;
+	}
+
+	public static int lt(final int x, final int y) {
+		return ((x - y) ^ ((x ^ y) & ((x - y) ^ x))) >>> 31;
+	}
+	
+	public static int gt(final int x, final int y) {
+		return lt(y, x);
+	}
+	
+	public static int le(final int x, final int y) {
+		return ((x | ~y) & ((x ^ y) | (~(y - x)))) >>> 31;
+	}
+
+	public static int ge(final int x, final int y) {
+		return le(y, x);
+	}
+
+	public static int doz(final int x, final int y) {
+		if (x >= y ) {
+			return x - y;
+		}
+		return 0;
+	}
+} 
--- src/test/java/bitutils/BasicsTest.java
+++ src/test/java/bitutils/BasicsTest.java
@@ -1,54 +1,191 @@
 package bitutils;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
 public class BasicsTest {
 	@Test
-	public void turnOffRightMost1Bit() {
+	public void turnOffRightmost1() {
 		int input  = 0b01011000;
 
-		input = Basics.turnOffRightMost1Bit(input);
+		input = Basics.turnOffRightmost1(input);
 		assertEquals(0b01010000, input);
 		
-		input = Basics.turnOffRightMost1Bit(input);
+		input = Basics.turnOffRightmost1(input);
 		assertEquals(0b01000000, input);
 		
-		input = Basics.turnOffRightMost1Bit(input);
+		input = Basics.turnOffRightmost1(input);
 		assertEquals(0b00000000, input);
 		
-		input = Basics.turnOffRightMost1Bit(input);
+		input = Basics.turnOffRightmost1(input);
 		assertEquals(0b00000000, input);
 	}
 	
 	@Test
-	public void turnOnRightMost0Bit() {
+	public void turnOnRightmost0() {
 		int input  = 0b10100111;
 
-		input = Basics.turnOnRightMost0Bit(input);
+		input = Basics.turnOnRightmost0(input);
 		assertEquals(0b10101111, input);
 		
-		input = Basics.turnOnRightMost0Bit(input);
+		input = Basics.turnOnRightmost0(input);
 		assertEquals(0b10111111, input);
 		
-		input = Basics.turnOnRightMost0Bit(input);
+		input = Basics.turnOnRightmost0(input);
 		assertEquals(0b11111111, input);
 	}
 	
 	@Test
 	public void turnOffTrailing1s() {
-		int input = 0b10100111;
-		
-		input = Basics.turnOffTrailing1s(input);
-		assertEquals(0b10100000, input);
+		assertEquals(0b10100000, Basics.turnOffTrailing1s(0b10100111));
 	}
 	
 	@Test
 	public void turnOnTrailing0s() {
-		int input = 0b10101000;
-		
-		input = Basics.turnOnTrailing0s(input);
-		assertEquals(0b10101111, input);
+		assertEquals(0b10101111, Basics.turnOnTrailing0s(0b10101000));
 	}
+	
+	@Test
+	public void markPositionOfRightmost0BitWith1() {
+		assertEquals(0b00001000, Basics.markPositionOfRightmost0With1(0b10100111));
+	}
+	
+	@Test
+	public void markPositionOfRightmost1BitWith0() {
+		assertEquals(0b11111111111111111111111111110111, Basics.markPositionOfRightmost1With0(0b10101000));
+	}
+	
+	@Test
+	public void createWordWith1sAtPositionsOfTrailing0s() {
+		assertEquals(0b00000111, Basics.createWordWith1sAtPositionsOfTrailing0s(0b01011000));
+	}
+	
+	@Test
+	public void createWordWith0sAtPositionsOfTrailing1s() {
+		assertEquals(0b11111111111111111111111111111000, Basics.createWordWith0sAtPositionsOfTrailing1s(0b10100111));
+	}
+	
+	@Test
+	public void markPositionOfRightmost1BitWith1() {
+		assertEquals(0b00001000, Basics.markPositionOfRightmost1BitWith1(0b01011000));
+	}
+	
+	@Test
+	public void markPositionOfRightmost1BitWith1WithNo1s() {
+		assertEquals(0b00000000, Basics.markPositionOfRightmost1BitWith1(0b00000000));
+	}
+	
+	@Test
+	public void markRightmost1WithContiguous1s() {
+		assertEquals(0b00001111, Basics.markRightmost1WithContiguous1s(0b01011000));
+	}
+	
+	@Test
+	public void markRightmost0WithContiguous1s() {
+		assertEquals(0b00001111, Basics.markRightmost0WithContiguous1s(0b01010111));
+	}
+	
+	@Test
+	public void clearRightmostContiguousStringOf1s() {
+		assertEquals(0b01000000, Basics.clearRightmostContiguousStringOf1s(0b01011100));
+	}
+	
+	@Test
+	public void isBaseTwoAMultipleOfBaseTwo() {
+		// These can be used to determine if a nonnegative integer is of the form 
+		// 2j − 2k for some j ≥ k≥ 0: apply the formula followed by a 0-test on the result.
+		assertEquals(true, Basics.isBaseTwoAMultipleOfBaseTwo(0b00001000, 0b00000010));
+		
+		assertEquals(false, Basics.isBaseTwoAMultipleOfBaseTwo(0b00001001, 0b00000010));
+		
+		assertEquals(false, Basics.isBaseTwoAMultipleOfBaseTwo(0b00001010, 0b00000101));
+		
+		try {
+			Basics.isBaseTwoAMultipleOfBaseTwo(0b00000010, 0b00001000);
+			fail("First argument cannot be greater than second.");
+		}
+		catch (IllegalArgumentException e) {
+		}
+		
+		try {
+			Basics.isBaseTwoAMultipleOfBaseTwo(0b11111111111111111111111111111111, 0b00001000);
+			fail("First operand has to be greater than zero.");
+		}
+		catch (IllegalArgumentException e) {
+		}
+	}
+	
+	@Test
+	public void abs() {
+		assertEquals(0b00000001, Basics.abs(0b11111111111111111111111111111111));
+	}
+	
+	@Test
+	public void nabs() {
+		assertEquals(0b11111111111111111111111111111111, Basics.nabs(0b00000001));
+	}
+	
+	@Test
+	public void avgRoundDown() {
+		assertEquals(2, Basics.floorAvg(2, 3));
+	}
+	
+	@Test
+	public void avgRoundUp() {
+		assertEquals(3, Basics.ceilingAvg(2, 3));
+	}
+	
+	@Test
+	public void threeValueCompare() {
+		assertEquals(-1, Basics.threeValueCompare(0, 1));
+		assertEquals(0, Basics.threeValueCompare(0, 0));
+		assertEquals(1, Basics.threeValueCompare(1, 0));
+	}
+	
+	@Test
+	public void comparisonPredicatesEquals() {
+		assertEquals(0, Basics.eq(0, 1));
+		assertEquals(0, Basics.eq(1, 0));
+		assertEquals(1, Basics.eq(1, 1));
+	}
+	
+	@Test
+	public void comparisonPredicatesNotEquals() {
+		assertEquals(0, Basics.ne(1, 1));
+		assertEquals(1, Basics.ne(1, 0));
+
+	}
+	
+	@Test
+	public void comparisonPredicatesLessThan() {
+		assertEquals(0, Basics.lt(0, 0));
+		assertEquals(1, Basics.lt(-1, 0));
+	}
+	
+	@Test
+	public void comparisonPredicatesGreaterThan() {
+		assertEquals(0, Basics.gt(0, 0));
+		assertEquals(1, Basics.gt(1, 0));
+	}
+	
+	@Test
+	public void comparisonPredicatesLessThanOrEqual() {
+		assertEquals(1, Basics.le(0, 0));
+		assertEquals(0, Basics.le(1, 0));
+	}
+
+	@Test
+	public void comparisonPredicatesGreaterThanOrEqual() {
+		assertEquals(1, Basics.ge(0, 0));
+		assertEquals(0, Basics.ge(-1, 0));
+	}
+	
+	@Test
+	public void doz() {
+		assertEquals(3, Basics.doz(7, 4));
+		assertEquals(0, Basics.doz(1, 2));
+	}
+
 }
